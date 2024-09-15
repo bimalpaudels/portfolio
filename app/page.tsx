b9cffc84f59f9c634eb013261a3633019f067bee
Bimal Paudel <ibimalp@gmail.com>
2024-09-15 11:26:09 +0200

    fix: some typescript fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import { fetchNotionPage } from "@/lib";
 import NotionRenderer from "@/app/renderer";
 
 export default async function Home() {
+  if (!process.env.NOTION_PAGE_ID) {
+    throw new Error("NOTION_PAGE_ID is not defined in environment variables.");
+  }
   const resp = await fetchNotionPage(process.env.NOTION_PAGE_ID);
+
   console.log(resp.results);
   return (
     <div>
