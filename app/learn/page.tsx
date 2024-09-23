9823d0dccf56e3625651f7bfe2c8ae4b4deaf75c
Bimal Paudel <ibimalp@gmail.com>
2024-09-23 15:01:53 +0200

    Feat: Learn page notion based

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,6 +1,9 @@
 import { fetchDatabaseContent } from "@/lib";
-import { NotionDBPagesRenderer } from "@/app/renderer";
+import NotionBlockChildrenRenderer, {
+  NotionDBPagesRenderer,
+} from "@/app/renderer";
 import { Header } from "@/app/componenets";
+import { fetchNotionPageContent } from "@/lib";
 
 export const metadata = {
   title: "Learn",
@@ -8,35 +11,25 @@ export const metadata = {
     canonical: "/learn",
   },
 };
+const learnPageIdd = process.env.LEARN_PAGE_ID;
+const notionDbId = process.env.NOTION_DB_ID;
 
-export default async function Home() {
-  if (!process.env.NOTION_DB_ID) {
+export const revalidate = 3600;
+
+export default async function Learn() {
+  if (!notionDbId) {
     throw new Error("NOTION_DB_ID is not defined in environment variables.");
   }
-  const db_content_response = await fetchDatabaseContent(
-    process.env.NOTION_DB_ID
-  );
+  if (!learnPageIdd) {
+    throw new Error("LEARN_PAGE_ID is not defined in environment variables.");
+  }
+  const learnPage = await fetchNotionPageContent(learnPageIdd);
+  const db_content_response = await fetchDatabaseContent(notionDbId);
   return (
     <>
       <Header />
-
-      <h2>Learnings</h2>
-      <div className="space-y-4">
-        <p>
-          This is the space where I will publish articles and snippets on all
-          things I learn and implement, hopefully for a long time.
-        </p>
-        <p>
-          Some articles are descriptive whereas, some are just code snippets and
-          may lack clarity.
-        </p>
-      </div>
-
-      <div className="mt-10 space-y-10">
-        <NotionDBPagesRenderer pages={db_content_response} />
-      </div>
+      <NotionBlockChildrenRenderer blocks={learnPage} />
+      <NotionDBPagesRenderer pages={db_content_response} />
     </>
   );
 }
-
-export const dynamic = "force-dynamic";
