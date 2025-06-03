16326a9da33726144425e8a15e76245b85fa4308
Bimal Paudel <ibimalp@gmail.com>
2025-06-03 15:43:20 +0200

    Fix: Removed unnecessary API call and componenet loaded

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,5 @@
-import { fetchDatabaseContent, fetchNotionPageContent } from "@/lib";
-import { PostsListRenderer, NotionBlockRenderer } from "@/components";
+import { fetchDatabaseContent } from "@/lib";
+import { PostsListRenderer } from "@/components";
 import { Header } from "@/components";
 
 export const metadata = {
@@ -8,22 +8,14 @@ export const metadata = {
     canonical: "/posts",
   },
 };
-const learnPageIdd = process.env.LEARN_PAGE_ID;
 
 export const revalidate = 300;
 
 export default async function Learn() {
-  if (!learnPageIdd) {
-    throw new Error("LEARN_PAGE_ID is not defined in environment variables.");
-  }
-  const learnPage = await fetchNotionPageContent(learnPageIdd);
   const db_content_response = await fetchDatabaseContent();
   return (
     <div className="animate-fade-in">
       <Header />
-      <div className="article">
-        <NotionBlockRenderer blocks={learnPage} />
-      </div>
       <div className="space-y-8 pt-8 animate-slide-in">
         <PostsListRenderer pages={db_content_response} />
       </div>
