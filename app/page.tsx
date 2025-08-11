b0fc78b989441e15a17d3dc1be0b75871ed56548
Bimal Paudel <ibimalp@gmail.com>
2025-08-11 11:29:38 +0200

    Feat: Improved landing page typography.

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import { Mail, Github, Linkedin, FileUser } from "lucide-react";
 export default function Home() {
   return (
     <div className="animate-fade-in">
-      <h2 className="font-heading font-semibold pt-12 text-gray-900 dark:text-gray-100">
+      <h4 className="font-heading font-semibold pt-12 text-gray-900 dark:text-gray-100">
         Bimal Paudel
-      </h2>
+      </h4>
 
-      <div className="article">
-        <p className="text-gray-900 dark:text-gray-100">
+      <div className="mb-2">
+        <p className="text-gray-700 dark:text-gray-300 leading-relaxed text-sm">
           I&apos;m a full-stack developer with over three years of experience
           building scalable web applications—based in Berlin, originally from
           Nepal.
